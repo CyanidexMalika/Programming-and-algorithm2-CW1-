d6f5c0544637dd747d3d12d00a02513174d3155e
CyanidexMalika <pradhan.samir1033@gmail.com>
2024-01-20 22:45:40 +0545

    Webcam capture

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import cv2
 
 
-# test_image=cv2.imread("photos/download.jpeg")
-# cv2.imshow("downloads",test_image)
-# cv2.waitKey(0)
-
-
-
-# capture=cv2.VideoCapture("photos/testvid.mp4")
-
 
 frameHeight = 600
 frameWidth = 700
 capture=cv2.VideoCapture(0)
-
-# capture.set(3,frameWidth)
 # capture.set(4,frameHeight)
 while True:
     success,frame=capture.read()
