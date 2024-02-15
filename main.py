184ab9e3d0cde27b698758417502affe87c0a1a8
CyanidexMalika <pradhan.samir1033@gmail.com>
2024-02-15 22:21:47 +0545

    Test after fix commit

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import cv2
 
-
-
 frameHeight = 600
-frameWidth = 700
-capture=cv2.VideoCapture(0)
-# capture.set(4,frameHeight)
+frameWidth = 900
+capture = cv2.VideoCapture(0)
+
 while True:
-    success,frame=capture.read()
-    frame=cv2.resize(frame,(frameWidth,frameHeight))
-    cv2.imshow("Test Video",frame)
+    success, frame = capture.read()
+    frame = cv2.resize(frame, (frameWidth, frameHeight))
+    
+    # Removing mirrored video from webcam part
+    frame = cv2.flip(frame, 1)
+    
+    cv2.imshow("Test Video", frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+capture.release()
+cv2.destroyAllWindows()
